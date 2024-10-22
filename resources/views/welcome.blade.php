b90918fd28b7d7774fb386a8b60f4d2c049dd289
Aryan-Mangla <manglaaryan05@outlook.com>
2024-10-22 21:27:31 +0530

    Card Animation added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,25 +14,33 @@
             <div class="row">
                 @foreach($events as $event)
                     <div class="col-md-4 mb-3">
-                    <div class="card position-relative event-card">
-    <!-- Event Image -->
-    <img src="{{ asset('images/events/' . ($event->images->first()->image_path ?? 'default-image.png')) }}" class="card-img-top" alt="Event Image">
-    <div class="card-body">
-        <!-- Event Title -->
-        <h5 class="card-title">{{ $event->title }}</h5>
-        <!-- Event Description (truncated) -->
-        <p class="card-text">{{ Str::limit($event->description, 100) }}</p>
-        <div class="d-flex justify-content-between">
-            <p class="card-text"><span class="theme-text">Start at:</span> {{ \Carbon\Carbon::parse($event->event_start_time)->format('d-m-Y') }}</p>
-            <p class="card-text"><span class="theme-text">End at:</span> {{ \Carbon\Carbon::parse($event->event_end_time)->format('d-m-Y') }}</p>
-        </div>
-        <p class="card-text"><span class="theme-text">Department: </span>{{ $event->department }}</p>
-        <!-- View Details Button -->
-        <a href="{{ route('public.events.show', $event->id) }}" class="btn btn-primary">View Details</a>
-    </div>
-    <div class="animated-underline"></div> <!-- Underline div -->
-</div>
+                    <div class="rotating-border">
+                    <div class="card event-card">
+                        <!-- Event Image -->
+                        <img src="{{ asset('images/events/' . ($event->images->first()->image_path ?? 'default-image.png')) }}" class="card-img-top" alt="Event Image">
 
+                        <!-- Card Body -->
+                        <div class="card-body">
+                            <!-- Event Title -->
+                            <h5 class="card-title">{{ $event->title }}</h5>
+
+                            <!-- Event Description -->
+                            <p class="card-text">{{ Str::limit($event->description, 100) }}</p>
+
+                            <!-- Event Start and End Times -->
+                            <div class="d-flex justify-content-between">
+                                <p class="card-text"><span class="theme-text">Start at:</span> {{ \Carbon\Carbon::parse($event->event_start_time)->format('d-m-Y') }}</p>
+                                <p class="card-text"><span class="theme-text">End at:</span> {{ \Carbon\Carbon::parse($event->event_end_time)->format('d-m-Y') }}</p>
+                            </div>
+
+                            <!-- Event Department -->
+                            <p class="card-text"><span class="theme-text">Department: </span>{{ $event->department }}</p>
+
+                            <!-- View Details Button -->
+                            <a href="{{ route('public.events.show', $event->id) }}" class="btn btn-primary">View Details</a>
+                        </div>
+                    </div>
+                </div>
                     </div>
                 @endforeach
             </div>
