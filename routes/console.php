4cccd197a077be56d94dd84d73880fe62221b022
Aryan-Mangla <manglaaryan05@outlook.com>
2024-10-23 21:57:55 +0530

    In console under routes added scheduler

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+})->purpose('Display an inspiring quote');
+
+Artisan::command('events:update-status', function () {
+    Artisan::call('events:update-status');
+})->hourly();
